b500d34b0a01af890a80e161b0a95c01e09ef684
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-31 16:45:53 -0300

    chore: add logout button container

--- client/src/containers/logout-button.tsx
+++ client/src/containers/logout-button.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
+import styled from "react-emotion";
+import { useApolloClient, gql } from "@apollo/client";
 
-const LogoutButton: React.FC<any> = () => {
-  return <div/>;
+import { menuItemClassName } from "../components/menu-item";
+import { ReactComponent as ExitIcon } from "../assets/icons/exit.svg";
+
+export default function LogoutButton() {
+  const client = useApolloClient();
+
+  const IS_LOGGED_IN = gql`
+    query IsUserLoggedIn {
+      isLoggedIn @client
+    }
+  `;
+
+  return (
+    <StyledButton
+      onClick={() => {
+        client.writeQuery({
+          query: IS_LOGGED_IN,
+          data: { isLoggedIn: true },
+        });
+        localStorage.clear();
+      }}
+    >
+      <ExitIcon />
+      Logout
+    </StyledButton>
+  );
 }
 
-export default LogoutButton;
+const StyledButton = styled("button")(menuItemClassName, {
+  background: "none",
+  border: "none",
+  padding: 0,
+});
