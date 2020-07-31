722aab2265bd88d83ff19712aeb513bd36e3db77
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-31 16:11:39 -0300

    chore: query local data to verify if user is signed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,27 +4,52 @@ import {
   InMemoryCache,
   NormalizedCacheObject,
   HttpLink,
+  useQuery,
+  gql,
 } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
 import injectStyles from "./styles";
+import { resolvers, typeDefs } from "./resolvers";
+import Login from "./pages/login";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: "http://localhost:4000/",
+  headers: { authorization: localStorage.getItem("token") },
 });
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link,
+  typeDefs,
+  resolvers,
 });
+
+
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+cache.writeQuery({
+  query: IS_LOGGED_IN,
+  data: { isLoggedIn: !!localStorage.getItem("token"), cartItems: [] },
+});
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
