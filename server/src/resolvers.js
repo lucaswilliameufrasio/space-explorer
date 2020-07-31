b1f42290b62cf4463d6dbd867a6672773fd32cb7
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-31 04:02:58 -0300

    chore: add mutation resolver to book trips and cancel a trip

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -48,7 +48,7 @@ module.exports = {
 
       // look up those launches by their ids
       return (
-        dataSources.launchAPI.getLaunchIdsByIds({
+        dataSources.launchAPI.getLaunchesByIds({
           launchIds,
         }) || []
       );
@@ -59,6 +59,42 @@ module.exports = {
     login: async (_, { email }, { dataSources }) => {
       const user = await dataSources.userAPI.findOrCreateUser({ email });
       if (user) return Buffer.from(email).toString("base64");
+    },
+
+    bookTrips: async (_, { launchsIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchsIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+
+      return {
+        success: results && results.length === launchesIds.length,
+        message:
+          results.length === launchIds.length
+            ? "trips booked successfully"
+            : `the following launches couldn't be booked: ${launchIds.filter(
+                (id) => !results.includes(id)
+              )}`,
+        launches,
+      };
+    },
+
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result)
+        return {
+          success: false,
+          message: "failed to cancel trip",
+        };
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      
+      return {
+        success: true,
+        message: "trip canceled",
+        launches: [launch],
+      };
     },
   },
 };
