54a226f4eeee44a4d4f368767cfa3d50f6026d3b
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-31 18:00:56 -0300

    chore: add update profile function

--- server/src/datasources/user.js
+++ server/src/datasources/user.js
@@ -1,5 +1,5 @@
-const { DataSource } = require('apollo-datasource');
-const isEmail = require('isemail');
+const { DataSource } = require("apollo-datasource");
+const isEmail = require("isemail");
 
 class UserAPI extends DataSource {
   constructor({ store }) {
@@ -66,7 +66,7 @@ class UserAPI extends DataSource {
       where: { userId },
     });
     return found && found.length
-      ? found.map(l => l.dataValues.launchId).filter(l => !!l)
+      ? found.map((l) => l.dataValues.launchId).filter((l) => !!l)
       : [];
   }
 
@@ -77,6 +77,42 @@ class UserAPI extends DataSource {
       where: { userId, launchId },
     });
     return found && found.length > 0;
+  }
+
+  /**
+   * This function is currently only used by the iOS tutorial to upload a
+   * profile image to S3 and update the user row
+   */
+  async uploadProfileImage({ file }) {
+    const userId = this.context.user.id;
+    if (!userId) return;
+
+    // Create new S3 client instance
+    const s3 = new S3();
+
+    /**
+     * Destructure mimetype and stream creator from provided file and generate
+     * a unique filename for the upload
+     */
+    const { createReadStream, mimetype } = await file;
+    const filename = uuidv4() + "." + mime.getExtension(mimetype);
+
+    // Upload the file to an S3 bucket using the createReadStream
+    const { AWS_S3_BUCKET } = process.env;
+    await s3
+      .upload({
+        ACL: "public-read", // This will make the file publicly available
+        Body: createReadStream(),
+        Bucket: AWS_S3_BUCKET,
+        Key: filename,
+        ContentType: mimetype,
+      })
+      .promise();
+
+    // Save the profile image URL in the DB and return the updated user
+    return this.context.user.update({
+      profileImage: `https://${AWS_S3_BUCKET}.s3.us-west-2.amazonaws.com/${filename}`,
+    });
   }
 }
 
