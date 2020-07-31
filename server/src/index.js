c7acbcbb10746a6e57f0f966db5f24ac2a40b9f0
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-31 03:47:15 -0300

    chore: add context function to handle authentication and call it on every graphql operation

--- server/src/index.js
+++ server/src/index.js
@@ -6,10 +6,24 @@ const resolvers = require("./resolvers");
 
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
+const IsEmail = require("isemail");
 
 const store = createStore();
 
 const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+
+    if (!IsEmail.validate(email)) return { user: null };
+
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
